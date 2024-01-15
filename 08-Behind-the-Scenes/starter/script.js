012fe0657f862773a2e823fd7ca6be2b23d01b74
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2024-01-15 18:36:41 +0100

    road to js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2024 - birthYear;
+
+    function printAge() {
+        const output = `${firstName} are ${age}, born in ${birthYear}`;
+        console.log(output);
+    }
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+        var millenial = true;
+        const str = `Oh, and you're a millenial, ${firstName}`;
+        console.log(str);
+    }
+
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Merlijn';
+calcAge(2000);
